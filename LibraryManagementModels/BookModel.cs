e36aab61c7f938c170c4ec21dd4878efb2de30cb
ohmybiino <davenarzobal@gmail.com>
2025-01-20 17:35:02 +0800

    Initial Models

---
+++ LibraryManagementModels/BookModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementModels
+{
+    public class BookModel
+    {
+        [Required]
+        [Key]
+        [StringLength(13, MinimumLength = 10, ErrorMessage = "ISBN must be 10 - 13 characters.")]
+        [DisplayName("ISBN")]
+        public string ISBN { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [DisplayName("Book Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DisplayName("Genre")]
+        public string Genre { get; set; }
+
+        [StringLength(200, MinimumLength = 2)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DisplayName("Author")]
+        public string Author { get; set; }
+
+        public bool isBorrowed { get; set; }
+    }
+}
---
+++ LibraryManagementModels/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementModels
+{
+    public class UserModel
+    {
+        [Key]
+        [Required]
+        [DisplayName("User ID")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100,MinimumLength =16, ErrorMessage ="Username must be at least 16 characters.")]
+        [DisplayName("Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("PhoneNumber")]
+        [StringLength(11,MinimumLength =11, ErrorMessage = "Phone number must be 11 characters.")]
+        public string PhoneNumber { get; set; }
+    }
+}
