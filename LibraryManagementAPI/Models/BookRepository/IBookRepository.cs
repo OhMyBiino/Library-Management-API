cc5f067371ba9ddb9dfc9b5f2a40577a17d55513
ohmybiino <davenarzobal@gmail.com>
2025-01-29 11:29:22 +0800

    Add GetBorrowedBooks in ITransactionRepository

--- LibraryManagementAPI/Models/BookRepository/IBookRepository.cs
+++ LibraryManagementAPI/Models/BookRepository/IBookRepository.cs
@@ -6,6 +6,7 @@ namespace LibraryManagementAPI.Models.BookRepository
     {
         Task<IEnumerable<BookModel>> GetBooksAsync();
         Task<BookModel> GetBookByIdAsync(int Id);
+        Task<IEnumerable<BookModel>> GetBorrowedBooksAsync();
         Task<IEnumerable<BookModel>> SearchAsync(string Name, string Genre);
         Task<BookModel> CreateBookAsync(BookModel bookModel);
         Task<BookModel> UpdateBookAsync(BookModel bookModel);
